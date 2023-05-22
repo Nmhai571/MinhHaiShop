dfbcaff707a45c2ca75bbf1a07cd28a577adbbb9
Minh Hai <nguyenminhhai572001@gmail.com>
2023-05-22 10:42:19 +0700

    first commit

---
+++ MinhHaiShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinhHaiShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
